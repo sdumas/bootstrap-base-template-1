a7c64541c1cba1957f5918f1912d4d5531691700
Vince Tardy <vtardy@syscomservices.com>
2014-12-04 14:50:34 -0500

    Revert "megamenu - initial implementation"
    This reverts commit 3dcb366fdeb182ecc750a79015cdf22e7edf929b.

--- js/custom.js
+++ js/custom.js
@@ -10,19 +10,5 @@ $(window).resize(function () {
   if ($(window).width() > 768) {
     $('.collapse, .main-content-container').removeClass('in');
   }
-
-  // STOP PROPAGATION SCRIPT
-
-  $(document).on('click', '.dropdown-menu', function(e) {
-    e.stopPropagation();
-  });
-
   // END ALL SCRIPTS
-});
-
-// Dropdown Menu Fade
-jQuery(document).ready(function(){
-  $(".dropdown").click(
-    function() { $('.dropdown-menu', this).fadeToggle("fast");
-});
 });
