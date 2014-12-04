3dcb366fdeb182ecc750a79015cdf22e7edf929b
Vince Tardy <vtardy@syscomservices.com>
2014-12-04 14:47:35 -0500

    megamenu - initial implementation
    glitches in off-canvas view/toggle, though

--- js/custom.js
+++ js/custom.js
@@ -10,5 +10,19 @@ $(window).resize(function () {
   if ($(window).width() > 768) {
     $('.collapse, .main-content-container').removeClass('in');
   }
+
+  // STOP PROPAGATION SCRIPT
+
+  $(document).on('click', '.dropdown-menu', function(e) {
+    e.stopPropagation();
+  });
+
   // END ALL SCRIPTS
+});
+
+// Dropdown Menu Fade
+jQuery(document).ready(function(){
+  $(".dropdown").click(
+    function() { $('.dropdown-menu', this).fadeToggle("fast");
+});
 });
